14b18bfb0512e2880971c59c40992ea295fecd59
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-11-06 18:18:34 -0500

    Minor tweaks to the sr cmd

--- dns-holdon.py
+++ dns-holdon.py
@@ -5,7 +5,7 @@ from scapy.all import *
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--server-ip", default="8.8.8.8", dest="server", help="IP of DNS server", required=True)
 parser.add_argument("-n", "--hostname", default="m.pvta.com", dest="hostname", help="The URL that you wish to perform a DNS lookup for", required=True)
-parser.add_argument("-t", "--timeout", default=5, dest="timeout", help="The amount of time (in seconds) to wait for a real response", required=True)
+parser.add_argument("-t", "--timeout", default=5, dest="timeout", help="The amount of time (in seconds) to wait for a real response", type=float, required=True)
 args = parser.parse_args()
 
 # First, send a non-sensitive query to a safe DNS resolver and pull the RTT and TTL for later
@@ -13,5 +13,5 @@ answer = sr1(IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname='m.pvta.com
 ttl = answer[IP].ttl
 
 
-answer = sr(IP(dst=args.server)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=args.hostname)), verbose=0, timeout=args.timeout, multi=True)
-print answer[DNS].summary()
+answered, unanswered = sr(IP(dst=args.server)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=args.hostname)), verbose=0, timeout=args.timeout, multi=True)
+print answered
