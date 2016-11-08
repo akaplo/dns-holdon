99e4f4b41acc262522365b7bd98952bf8fce43a2
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-11-07 20:02:47 -0500

    Pretty much figured out how to get the dns records parsed and determined to be legit/not.

--- dns-holdon.py
+++ dns-holdon.py
@@ -1,17 +1,27 @@
-# python dns-holdon.py -s 8.8.8.8 -n m.pvta.com
+# python dns-holdon.py -s 130.245.145.6 -n falun.com -t 15
 import argparse
 from scapy.all import *
+import pdb
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--server-ip", default="8.8.8.8", dest="server", help="IP of DNS server", required=True)
-parser.add_argument("-n", "--hostname", default="m.pvta.com", dest="hostname", help="The URL that you wish to perform a DNS lookup for", required=True)
+parser.add_argument("-s", "--server-ip", default="130.245.145.6", dest="server", help="IP of DNS server", required=True)
+parser.add_argument("-n", "--hostname", default="falun.com", dest="hostname", help="The URL that you wish to perform a DNS lookup for", required=True)
 parser.add_argument("-t", "--timeout", default=5, dest="timeout", help="The amount of time (in seconds) to wait for a real response", type=float, required=True)
 args = parser.parse_args()
 
 # First, send a non-sensitive query to a safe DNS resolver and pull the RTT and TTL for later
-answer = sr1(IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname='m.pvta.com')), verbose=0)
-ttl = answer[IP].ttl
+answer = sr1(IP(dst='130.245.145.6')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname='m.pvta.com')), verbose=0)
+peepee_el = answer[IP].ttl
 
 
-answered, unanswered = sr(IP(dst=args.server)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=args.hostname)), verbose=0, timeout=args.timeout, multi=True)
-print answered
+answers, nonanswers = sr(IP(dst=args.server)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=args.hostname)), verbose=0, timeout=args.timeout, multi=True)
+for answer in answers:
+    #pdb.set_trace()
+    if answer[-1][IP].ttl != peepee_el:
+        print 'injection!'
+    # answer is a tuple.  Last entry in the tuple contains
+    # the dns record, which contains the actual response.
+    else:
+        print answer[-1][DNSRR].rdata
+#print answer.IP
+#print answered[DNS][1]
