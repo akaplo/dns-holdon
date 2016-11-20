4f546c384e75c6bcbc546348663598bf0adb8089
Aaron Kaplowitz <akaplowitz@umass.edu>
2016-11-19 20:46:10 -0500

    Commented

--- dns-holdon.py
+++ dns-holdon.py
@@ -16,6 +16,7 @@ import pdb
 import time
 import math
 
+# Prepare the 3 required params
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--server-ip", default="130.245.145.6", dest="server", help="IP of DNS server", required=True)
 parser.add_argument("-n", "--hostname", default="falun.com", dest="hostname", help="The URL that you wish to perform a DNS lookup for", required=True)
@@ -23,32 +24,34 @@ parser.add_argument("-t", "--timeout", default=5, dest="timeout", help="The amou
 args = parser.parse_args()
 
 # First, send a non-sensitive query to a safe DNS resolver and pull the RTT and TTL for later
+# Timestamp for when we sent the query; used for calculating RTT
 non_sensitive_send_time = time.time()
 answer = sr1(IP(dst='130.245.145.6')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname='m.pvta.com')), verbose=0)
+# The RTT of the non-sensitive query
 non_sensitive_rtt = answer.time - non_sensitive_send_time
+# The TTL of the non-sensitive query
 non_sensitive_ttl = answer[IP].ttl
 
+# Construct a DNS query, send it, and wait a configurable amount of time for
+# a response.
 answers, nonanswers = sr(IP(dst=args.server)/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=args.hostname)), verbose=0, timeout=args.timeout, multi=True)
+# We only care about the answers to our query.  Let's look at each one:
 for answer in answers:
+    # Determine the RTT for the entire send/receive of this packet.
     sensitive_send_time = answer[0].sent_time
     sensitive_recv_time = answer[-1].time
     sensitive_rtt = sensitive_recv_time - sensitive_send_time
-    # print 'sensitive'
-    # print sensitive_rtt
-    # print 'half non sensitive'
-    # print non_sensitive_rtt / 2
-    rtt_difference = math.fabs(sensitive_rtt - non_sensitive_rtt)
+    # DNS Hold-On specifies that a response received in
+    # less than half of the expected time was injected.
     # This line should actually be
-    # > if answer[-1][IP].ttl != non_sensitive_ttl and sensitive_rtt <= non_sensitive_rtt/2
+    # > if answer[-1][IP].ttl != non_sensitive_ttl and sensitive_rtt < non_sensitive_rtt/2
     # however, our testing server sends all responses with the same TTL.
-    print sensitive_rtt
-    print non_sensitive_rtt/2
-    print answer[-1][DNS].summary()
     if sensitive_rtt < non_sensitive_rtt/2:
         print 'An evil DNS resolver tried to lie and tell you that ' + args.hostname + ' is at ' + answer[-1][DNSRR].rdata + ' but it\'s not!'
-    # answer is a tuple.  Last entry in the tuple contains
-    # the dns record, which contains the actual response.
+    # Otherwise, we can say the response was a legitimate.
     else:
+        # answer is a tuple.  Last entry in the tuple contains
+        # the dns record, which contains the actual response.
         print args.hostname + ' is actually at ' + answer[-1][DNSRR].rdata
 #print answer.IP
 #print answered[DNS][1]
